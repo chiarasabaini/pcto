c6914eda7759030fd38e0eb04174dc5bec4245b0
chiarasabaini <chiara@sabaini.com>
2021-03-01 17:52:57 +0100

    jquery

--- res/script/script.js
+++ res/script/script.js
@@ -1,1 +1,8 @@
 console.log('hello, world!');
+
+/*
+$("#content").hide();
+$("#content").show();
+$('#content').css({'background-color' : '#ffffff', 'color' : '#000000'});
+$('#content').css({'background-color' : '#000000', 'color' : '#ffffff'});
+ */
